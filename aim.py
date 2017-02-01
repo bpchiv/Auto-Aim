94dcf8177189db32a5058f448fcf8f0b1cbefb32
Brian <bpchiv@gmail.com>
2017-02-01 17:58:20 -0500

    Image finding
    playing around with image finding using pyautogui and opencv

--- aim.py
+++ aim.py
@@ -1,4 +1,11 @@
 import pyautogui
-screenWidth, screenHeight = pyautogui.size()
-currentMouseX, currentMouseY = pyautogui.position()
-pyautogui.moveTo(100,150)
+im1=pyautogui.screenshot()
+im1.save('my_screenshot.png')
+location = pyautogui.locate('images/Heroes/Widow/widow_head.png','my_screenshot.png', grayscale=False)
+
+print(location)
+
+#widow_location = pyautogui.locateOnScreen('images/Heroes/Widow/widow_head.png')
+#print(widow_location)
+#widowX,widowY = pyautogui.center(widow_location)
+#pyautogui.moveTo(widowX,widowY)
